85d4dd8b605c9cea1aa20579024391f85cbf5cf4
jiaQiu9 <351470636@qq.com>
2021-11-13 11:23:21 -0500

    created a main file

---
+++ project/src/Main.java
@@ -0,0 +1,10 @@
+// @author: Jiasheng Qiu
+// Project description: This is project is for the HackHolyoke hackthon
+// the projects addresses the topic/ challenge of Organization and scheduling hack
+//
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+    }
+}
